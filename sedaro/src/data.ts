321aa41eeee3733ff9eeed1543c024ad56a6be90
Bas Welsh <sebastian.welsh@sedarotech.com>
2022-12-19 13:01:59 -0500

    Added latency to data mock

--- sedaro/src/data.ts
+++ sedaro/src/data.ts
@@ -7,22 +7,24 @@ export const getData = (
     if (start < 0 || start >= stop || width <= 0) {
       reject({ error: 'Invalid range' });
     }
-    if (Math.random() > 0.7) {
-      reject({ error: 'Unknown error' });
-    }
-    const data: { [key: string]: number[] } = {
-      t: [],
-      x: [],
-      y: [],
-      z: [],
-    };
-    for (let i = start; i <= stop; i += width) {
-      const r = (i % 1) * 2 * Math.PI;
-      data.t.push(i);
-      data.x.push(Math.sin(r));
-      data.y.push(Math.cos(r));
-      data.z.push(Math.tan(r));
-    }
-    resolve(data);
+    setTimeout(() => {
+      if (Math.random() > 0.7) {
+        reject({ error: 'Unknown error' });
+      }
+      const data: { [key: string]: number[] } = {
+        t: [],
+        x: [],
+        y: [],
+        z: [],
+      };
+      for (let i = start; i <= stop; i += width) {
+        const r = (i % 1) * 2 * Math.PI;
+        data.t.push(i);
+        data.x.push(Math.sin(r));
+        data.y.push(Math.cos(r));
+        data.z.push(Math.tan(r));
+      }
+      resolve(data);
+    }, ((stop - start) / width) * 0.5 + Math.random() * 1000); // Pseudo-random, load-dependent latency
   });
 };
